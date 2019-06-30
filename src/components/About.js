cebd05167b18bb9f7323160265c35d9415d9b0c7
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-06-30 14:23:40 +0400

    Add icon and styles in about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,15 @@ function About(){
     return(
         <section className="about-us" id="about">
         <h1>About</h1>
+        <p className="about-para">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+       
+        <p className="about-para">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
         </section>
     )
 }
--- src/components/Features.js
+++ src/components/Features.js
@@ -9,19 +9,19 @@ function Features(){
         <div className="feature-card">
         <img src="https://img.icons8.com/ios/50/000000/microphone.png" />
         <h2>Speech to text</h2>
-        <p>A record button everywhere, ready to go when you need it.  Unlimited recording time, background recording and pause / resume make it the perfect recorder.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
         
         </div>
         <div className="feature-card">
-        <img src="https://img.icons8.com/ios/50/000000/microphone.png"/>
-        <h2>Speech to text</h2>
-        <p>A record button everywhere, ready to go when you need it.  Unlimited recording time, background recording and pause / resume make it the perfect recorder.</p>
+        <img src="https://img.icons8.com/ios/80/000000/edit-property.png" />
+        <h2>Editable</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
         
         </div>
         <div className="feature-card">
-        <img src="https://img.icons8.com/ios/50/000000/microphone.png"/>
-        <h2>Speech to text</h2>
-        <p>A record button everywhere, ready to go when you need it.  Unlimited recording time, background recording and pause / resume make it the perfect recorder.</p>
+        <img src="https://img.icons8.com/dotty/80/000000/share.png" />
+        <h2>Share</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
         
         </div>
         </div>

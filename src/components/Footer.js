2fe9f0e4b22bc9f52c973367b4a158d551b163c1
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-02 22:35:13 +0400

    Add github and linkedin icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './Footer.css';
+// var React = require('react')
+var FontAwesome = require('react-fontawesome');
 
-function Footer (){
-    return(
+// var MyComponent = React.createClass({
+// render: function() {
+// function Footer (){
+//     return(
      
+//         <footer className="footer">
+//         <FontAwesome
+//         className="super-crazy-colors"
+//         name="rocket"
+//         size="2x"
+//         spin
+//         style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+//       />
+       
+//         <p>Copyright &copy; {new Date().getFullYear()} </p>
+//         </footer>
+//     )
+// }
+// }
+// })
+function Footer(){
+    return(
         <footer className="footer">
-        <p>Copyright &copy; {new Date().getFullYear()} </p>
-        </footer>
+         <FontAwesome
+        className="icon"
+         name="github"
+         size="2x"
+      />
+       <FontAwesome
+        className="icon"
+         name="linkedin"
+         size="2x"
+      />
+       
+         <p>Copyright &copy; {new Date().getFullYear()} </p>
+         </footer>
     )
 }
-
-
 export default Footer;

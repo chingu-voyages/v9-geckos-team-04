bd144570f34adfc49939398836490911665279c4
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-06 21:30:24 +0400

    Fix footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,30 +7,27 @@ function Footer(){
     return(
   
         <footer className="footer">
-          <div className="profiles">
-          <div className="creators-section">
-          <h2 className="creators">Creators</h2>
-          </div>
-          <div className="pablo">
-          <h3><a href="https://github.com/pablowbk" target="_blank">Pablo Wiebek</a></h3>
-            <FontAwesome
+        <h2 className="creators">Development Team</h2>
+          <div className="profiles">      
+          <div className="profile">
+              <FontAwesome
               className="icon"
               name="github"
               size="2x"
             /> 
+            <a href="https://github.com/pablowbk" target="_blank">Pablo Wiebek</a>
           </div>
 
-          <div className="profiles">
-          <div className="hemlata">
-          <h3><a href="https://github.com/Hemlatab" target="_blank">Hemlata Bhundhoo</a></h3>
+          <div className="profile">
             <FontAwesome
               className="icon"
               name="github"
               size="2x"
             />
+            <a href="https://github.com/Hemlatab" target="_blank">Hemlata Bhundhoo</a>
           </div>
         </div>
-        </div>
+        <br/>
 
           <p>Copyright &copy; {new Date().getFullYear()} </p>
 

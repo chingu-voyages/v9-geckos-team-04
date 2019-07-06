f628564419881e9b87407b926b8e86bc24f2d2c1
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-06 17:33:03 +0400

    Add creators github profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,40 +5,34 @@ var FontAwesome = require('react-fontawesome');
 
 function Footer(){
     return(
+  
         <footer className="footer">
           <div className="profiles">
-            
+          <div className="creators-section">
+          <h2 className="creators">Creators</h2>
+          </div>
           <div className="pablo">
+          <h3><a href="https://github.com/pablowbk" target="_blank">Pablo Wiebek</a></h3>
             <FontAwesome
               className="icon"
               name="github"
               size="2x"
-            />
-            <FontAwesome
-              className="icon"
-              name="linkedin"
-              size="2x"
-            />
+            /> 
           </div>
 
-
+          <div className="profiles">
           <div className="hemlata">
+          <h3><a href="https://github.com/Hemlatab" target="_blank">Hemlata Bhundhoo</a></h3>
             <FontAwesome
               className="icon"
               name="github"
               size="2x"
             />
-            <FontAwesome
-              className="icon"
-              name="linkedin"
-              size="2x"
-            />
-
           </div>
+        </div>
         </div>
 
           <p>Copyright &copy; {new Date().getFullYear()} </p>
-
 
         </footer>
     )

55c744b09f3a3467e8796c5aa9a45bd068d5a35f
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-06 15:49:02 +0400

    Add fontawesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import './Footer.css';
-// var React = require('react')
+
 var FontAwesome = require('react-fontawesome');
 
-// var MyComponent = React.createClass({
-// render: function() {
-// function Footer (){
-//     return(
-     
-//         <footer className="footer">
-//         <FontAwesome
-//         className="super-crazy-colors"
-//         name="rocket"
-//         size="2x"
-//         spin
-//         style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-//       />
-       
-//         <p>Copyright &copy; {new Date().getFullYear()} </p>
-//         </footer>
-//     )
-// }
-// }
-// })
 function Footer(){
     return(
         <footer className="footer">

63c5148fab45d7b73db15b527d4bb2373f21efaa
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-07-06 17:34:14 +0400

    Add content to about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,11 @@ function About(){
         <section className="about-us" id="about">
         <h1>About</h1>
         <p className="about-para">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Vext - Speech to text is a web recognition app which offers recording to any language by just clicking on the 'Start Recording' button. Vext is accessible on any devices, so you can capture moments using iPhone, iPad, and Mac. Using the share icons you can let people know of your moments. Don't let any of your idea go - capture it by using Vext and share it to the world.
         </p>
        
         <p className="about-para">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Vext offers three amamzing features; speech to text, copy to clipboard and share facility. Easier way to learn new languages using VEXT it wil help translate your native language to your chosen language. You have the facility to copy the voice trancription to your chosen editor. Share your thoughts to the world. Vext gives you the freedom to capture your thoughts from anywhere and on any device.
         </p>
         </section>
     )

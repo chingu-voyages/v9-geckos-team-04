eafd450bb639689fafe5d84e570eeeb3dc71bebc
pablowbk <pablo.weisbek@gmail.com>
2019-07-08 10:41:30 -0300

    add share btns animation

--- src/components/Share.js
+++ src/components/Share.js
@@ -11,59 +11,61 @@ import {
   EmailIcon,
 } from 'react-share';
 
+
 const Share = ({text}) => {
-  const shareUrl = "https://vext.netlify.com";
+  const shareUrl = "https://chingu-v9-geckos-04.netlify.com/";
   const title = text;
+  const btnStyle = text ? "share-btn" : "share-btn share-hidden";
 
   return (
     <div className="Share">
-      <div className="share-btn">
+      <div className={btnStyle}>
         <FacebookShareButton
           url={shareUrl}
           quote={title}
-          className="facebook">
+        className="facebook">
           <FacebookIcon
-            size={32}
-            round />
+            size={48}
+          round />
         </FacebookShareButton>
       </div>
 
-      <div className="share-btn">
+      <div className={btnStyle}>
         <TwitterShareButton
           url={shareUrl}
           title={title}
-          className="twitter">
+        className="twitter">
           <TwitterIcon
-            size={32}
-            round />
+            size={48}
+          round />
         </TwitterShareButton>
       </div>
 
-      <div className="share-btn">
+      <div className={btnStyle}>
         <WhatsappShareButton
           url={shareUrl}
           title={title}
-          separator="\n "
-          className="whatsapp">
-          <WhatsappIcon size={32} round />
+          separator="'\n'"
+        className="whatsapp">
+          <WhatsappIcon size={48} round />
         </WhatsappShareButton>
       </div>
 
-      <div className="share-btn">
-         <EmailShareButton
+      <div className={btnStyle}>
+        <EmailShareButton
           url={shareUrl}
-          subject="Speech to text by VEXT"
+          subject="New Voice Message transcribed using VEXT"
           body={text}
-          className="email">
+        className="email">
           <EmailIcon
-            size={32}
+            size={48}
             round />
         </EmailShareButton>
       </div>
 
-      
 
-    </div> 
+
+    </div>
   );
 }
 
